7162318fb9a98a2e5504add2c665bcf6e01a2aa1
Spencer Sliffe <spenceronw@ku.edu>
2022-09-30 11:18:03 -0500

    Added database for user information
    Added Sqlite to all the projects to create a database for all of the user sign in information. Information that is entered on the registration page is added to the database assigned to variables in the form of a table. Should allow for an easy file to search for user information when they login.

---
+++ PalsPolls/PalsPolls/Views/RegisterPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using PalsPolls.Tables;
+using SQLite;
+using Xamarin.Forms;
+
+namespace PalsPolls
+{
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDataBase.db");
+            var db = new SQLiteConnection(dbpath);
+            db.CreateTable<RegUserTable>();
+
+            var item = new RegUserTable()
+            {
+                UserName = EntryUserName.Text,
+                Password = EntryUserPassword.Text,
+                Email = EntryUserEmail.Text,
+                PhoneNumber = EntryUserPhoneNumber.Text
+            };
+
+            db.Insert(item);
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Congradulations", "User Registration Successful", "Yes", "Cancel");
+            });
+        }
+    }
+}
+
