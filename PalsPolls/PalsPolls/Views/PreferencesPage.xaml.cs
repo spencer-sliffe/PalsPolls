1cb7e06a066b30e8038f1338e33ecd02660b75de
Spencer Sliffe <spenceronw@ku.edu>
2022-10-12 14:06:55 -0500

    Database information is being displayed in preference page finally!

--- PalsPolls/PalsPolls/Views/PreferencesPage.xaml.cs
+++ PalsPolls/PalsPolls/Views/PreferencesPage.xaml.cs
@@ -6,6 +6,7 @@ using SQLite;
 using Xamarin.Forms;
 using PalsPolls.Services;
 using System.Threading.Tasks;
+using System.Net.NetworkInformation;
 
 namespace PalsPolls.Views
 {
@@ -18,29 +19,37 @@ namespace PalsPolls.Views
             m_UserTable = MyAccount;
             String myUserName = UserName(m_UserTable);
             DisplayUN.Text = myUserName;
-            
+            String myEmail = Email(m_UserTable);
+            DisplayEM.Text = myEmail;
+            String myPhoneNumber = PhoneNumber(m_UserTable);
+            string formattedPhoneNumber = string.Format("({0}) {1}-{2}", myPhoneNumber.Substring(0, 3), myPhoneNumber.Substring(3, 3), myPhoneNumber.Substring(6, 4));
+            DisplayPN.Text = formattedPhoneNumber;
         }
-
-        
 
         async void Button_Clicked(System.Object sender, System.EventArgs e)
         {
-
             await Navigation.PushAsync(new SignIn());
         }
-
 
         async void ViewCell_Tapped_1(System.Object sender, System.EventArgs e)
         {
             await Navigation.PushAsync(new SignIn());
         }
 
-
         private String UserName(RegUserTable MyAccount)
         {
             return MyAccount.UserName;
         }
+        
+        private String Email(RegUserTable MyAccount)
+        {
+            return MyAccount.Email;
+        }
 
+        private String PhoneNumber(RegUserTable MyAccount)
+        {            
+            return MyAccount.PhoneNumber;
+        }
     }
 }
 
